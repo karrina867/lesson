9cd7c8b1239fc002fcdb5658219610cf89f45ce3
karrina867 <92594854+karrina867@users.noreply.github.com>
2022-10-24 18:39:49 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//даго целое число m  вывести m  раз символ @
+//	int m;
+//	cin >> m;
+	//for (int i = 1; i <= m; i += 1) {
+	//	cout << '@';
+	//}
+	//дано два чесла x и y (вводим x < y).найти сумму четных чисел // чисел из диапозона 
+	//int x, y, i, sum = 1;
+	//cout << "vvedite x: ";
+	//cin >> x;
+	//cout << "vvedite y: ";
+	//cin >> y;
+	//i = x;
+	//while (i <= y) {
+	//	if (i % 2 == 0) {
+	//		sum += i;		
+	//	}
+	//	i++;	}
+	//cout << sum << endl;
+	// дано два числа x и  y (вводим x < y). напечатать нечетные числа из диапазона 
+	//int x, y, i;
+	//cout << "vvedite x: ";
+	//cin >> x;
+	//cout << "vvedite y: ";
+	//cin >> y;
+	//i = x;
+	//while (i >= y) (i <= 1);7 {
+	//	if (i % 2 == 1) {
+
+		//}
+		//i++;//	}
+//	int m;
+		//cin >> m;
+	//	for (int i = 0; i < 20; i++) cout << '#';
+	//	cout << endl;
+	//	for (int i = 0; i < 6; i++)
+	//	{ 
+	//	for (int i = ; i <= m; i += 1) {
+		//	cout << '@';
+		//}
+	int x, y, i, sum=0;
+	i = 1;
+	
+	
+		cout << "vvedite x: ";
+		cin >> x;
+	while (i <= x) {
+		if (i % 3 == 0) {
+			sum += i;
+		}
+		i++
+	}
+	cout << sum;
+//18
+	//int n;
+	//cout << "vvedite N";
+	//cin >> n;
+	//for (int i = 1; i<= n; i += 1) {
+	//	if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+		//	cout << i << endl;
+		//}
+//	19
+}
